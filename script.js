db4359cf58ebf7305afb3b29f92b54f62059129d
ankitjhagithub21 <ajha45075@gmail.com>
2024-07-26 19:09:51 +0530

    added scrolltrigger

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
-gsap.from('#page2 h1',{
-    y:100,
-    delay:0.3,
-    duration:0.5,
-    
-    opacity:0
-})
 
-gsap.from('#page2 span',{
-    y:30,
-    delay:0.5,
-    duration:0.5,
-    stagger:0.5,
-    opacity:0
-})
+gsap.registerPlugin(ScrollTrigger);
+
+function page2Animation() {
+    ScrollTrigger.create({
+        trigger: '#page2',
+        start: 'top center',
+        onEnter: () => {
+            gsap.from('#page2 h1', {
+                y: 100,
+                duration: 0.5,
+                opacity: 0
+            });
+        
+            gsap.from('#page2 span', {
+                y: 30,
+                duration: 0.5,
+                stagger: 0.5,
+                opacity: 0
+            });
+        
+            gsap.to('.line', {
+                width: "100%",
+                duration: 2
+            });
+        },
+        onEnterBack: () => {
+            gsap.from('#page2 h1', {
+                y: 100,
+                duration: 0.5,
+                opacity: 0
+            });
+        
+            gsap.from('#page2 span', {
+                y: 30,
+                duration: 0.5,
+                stagger: 0.5,
+                opacity: 0
+            });
+        
+            gsap.to('.line', {
+                width: "100%",
+                duration: 2
+            });
+        }
+    });
+}
 
 
-gsap.to('.line',{
-    width:"100%",
-    duration:2,
-    delay:0.3
-})
+page2Animation();
